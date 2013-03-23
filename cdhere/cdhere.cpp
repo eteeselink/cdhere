abaf6f268741dd46a861b944ad8fce19c8cb86bb
teeselinke <egbert.teeselink@sioux.eu>
2013-03-23 14:04:52 +0100

    help info

--- cdhere/cdhere.cpp
+++ cdhere/cdhere.cpp
@@ -17,11 +17,34 @@ public:
     }
 };
 
+std::vector<String> getArgs(int argc, _TCHAR* argv[])
+{
+    std::vector<String> args;
+    for(int i = 0; i < argc; i++)
+    {
+        args.push_back(argv[i]);
+    }
+    return args;
+}
+
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 
     try
     {
+        auto args = getArgs(argc, argv);
+        
+        if(args.size() == 2)
+        {
+            String& arg = args[1];
+            if((arg == "-h") || (arg == "--help") || (arg == "/h"))
+            {
+                printf("Prints the location of the topmost Windows Explorer window to STDOUT");
+                return 2;
+            }
+        }
+
         COMNess com;
 
         auto windows = getOrderedWindows();
@@ -32,7 +55,7 @@ int _tmain(int argc, _TCHAR* argv[])
     catch(Exception e)
     {
         e.print();
-        exit(1);
+        return 1;
     }
     
 	return 0;
